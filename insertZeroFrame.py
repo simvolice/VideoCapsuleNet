a7af43c9fc8526fef8dd90f1224b1298af932176
simvolice <simvolice@gmail.com>
2020-07-06 17:49:09 +0600

    feat: добавил удаление фрэймов для выравнивания батчей

--- insertZeroFrame.py
+++ insertZeroFrame.py
@@ -1,9 +1,20 @@
 import shutil
 import os
+
 outdir_clips = "/root/PycharmProjects/VideoCapsuleNet/UCF101_Frames/frames/"
 
 allFrameDirs = os.listdir(outdir_clips)
 
-for dirClip in allFrameDirs:
-    shutil.copyfile(os.path.join(outdir_clips, dirClip, "frame_1.jpg"), os.path.join(outdir_clips, dirClip, "frame_0.jpg"))
-    print("copy...")
+
+def insertZeroFrame():
+    for dirClip in allFrameDirs:
+        shutil.copyfile(os.path.join(outdir_clips, dirClip, "frame_1.jpg"),
+                        os.path.join(outdir_clips, dirClip, "frame_0.jpg"))
+        print("copy...")
+
+
+def deleteFramesOutOfRange():
+    for dirClip in allFrameDirs:
+        for deleteFilesNumber in range(40, len(os.listdir(os.path.join(outdir_clips, dirClip)))):
+            os.remove(os.path.join(outdir_clips, dirClip, "frame_{}.jpg".format(deleteFilesNumber)))
+            print("delete...")
